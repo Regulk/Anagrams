7fdcd5fb88369f5114a982863c6800031965f190
Regulk <105784171+Regulk@users.noreply.github.com>
2022-05-28 20:53:41 +0100

    Add files via upload

---
+++ PythonClassZuri- Anagrams/main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+name = "Michael"
+String = "I am a boy"
+new = "mike b"
+# print(" His name is " + name)
+# print(len(name))
+# print(String.count(''))
+# print(String.endswith('I'))
+# age = 16
+# movie = "Samurai"
+
+# if age == 18:
+# print(" You can watch this " + movie)
+# if age < 18:
+# print(" You are not allowed to watch  %s" % movie
+
+String1 = input(" String1: ")
+String2 = input(" String2: ")
+sort_String1 = sorted(String1)
+sort_String2 = sorted(String2)
+
+if sort_String1 == sort_String2:
+    print("True")
+else:
+    print("false")
